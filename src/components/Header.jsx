10e73b33e9967ff81d34b79e5384c73f4e9223a3
Andres Linan <andreseduardo21010@gmail.com>
2022-07-25 16:29:07 -0500

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
-import Menu from '@components/Menu';
+import Menu from '../components/Menu';
+import AppContext from '../context/AppContext';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
     const [toggle, setToggle] = useState(false);
+    const { state: {cart} } = useContext(AppContext)
 
     const handleToggle = () => {
         setToggle(!toggle);
@@ -45,7 +47,7 @@ const Header = () => {
                     </li>
                     <li className="navbar-shopping-cart">
                         <img src={shoppingCart} alt="shopping cart" />
-                        <div>2</div>
+                        {cart.length > 0 ? <div>{cart.length}</div> : null}
                     </li>
                 </ul>
             </div>
